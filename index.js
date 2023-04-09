bd0e7eb358c2b0cf960a0edc109cbdd8eed42b9e
duxpixels <duxwork0@gmail.com>
2023-04-09 21:07:19 +0200

    add capitalize function

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const randArr = require("./lib/randArr");
+const capitalize = require("./lib/capitalize")
 
 const agere = {
   randArr,
+  capitalize,
 };
 
 module.exports = agere;
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,10 @@
-const { randArr } = require("../index");
+const { randArr, capitalize } = require("../index");
 
 describe("Agere", () => {
   test("randArr", () => {
     expect(randArr(["a", "b", "c"])).not.toBe(["a", "b", "c"]);
+  });
+  test("capitalize", () => {
+    expect(capitalize("egypt")).toBe("Egypt");
   });
 });
